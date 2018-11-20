7783b44bfd6a8878e1c2d6e5f546662964440c61
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2018-11-20 15:39:47 -0300

    FIX: Parametro linea

--- conf/config.inc.php
+++ conf/config.inc.php
@@ -50,5 +50,5 @@
         // check if is a valid error
         if (error_get_last() !== null && in_array($error->type, [ E_ERROR, E_PARSE, E_COMPILE_ERROR ]))
             // redirect to error handler
-            _default_error_handler($error->type, $error->message, $error->file, $error->file);
+            _default_error_handler($error->type, $error->message, $error->file, $error->line);
     });
